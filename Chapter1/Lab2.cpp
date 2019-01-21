8300527aa4f13550929ee2ede13cea7077122478
Lucian Dinu <lucian.dinu93@yahoo.com>
2019-01-21 19:08:52 +0200

    Initial commit

---
+++ Chapter1/Lab2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+
+int main(void)
+{
+    int v = 10800; // 3 * 60 * 60
+    int three_min = 3 * 60; // 3 minutes in seconds
+    int six_min = 6 * 60; // 6 minutes in seconds
+    float pi = 3.141526; // value of pi
+
+    std::cout << "value of v= " << v << std::endl;
+
+    return 0;
+}
