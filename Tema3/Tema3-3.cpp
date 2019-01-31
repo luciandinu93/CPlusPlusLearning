bb86de3cfbfd305d171069191bb5178aaef1f8be
Lucian Dinu <lucian.dinu93@yahoo.com>
2019-01-31 20:38:13 +0200

    Initial commit

--- Tema3/Tema3-3.cpp
+++ Tema3/Tema3-3.cpp
@@ -1,50 +1,144 @@
 /* Se da un sir de n numere naturale citite de la tastatura.
  * Sa se salveze numerele intr-un vector de numere intregi si apoi afisati:
- *
- * 1. Elementele vectorului
- * 2. Elementele vectorului aflate pe pozitii pare
- * 3. Elementele impare ale vectorului
- * 4. Suma elementelor pozitive
- * 5. Produsul elementelor negative aflate pe pozitii impare;
- * 6. Numarul elementelor divizibile cu 5
- * 7. Daca elem de pe pozitia p (p citit de la tastatura) este par
+ * 1. elementele vectorului
+ * 2. elementele vectorului aflate pe pozitii pare
+ * 3. elementele impare ale vectorului
+ * 4. suma elementelor pozitive
+ * 5. produsul elementelor negative aflate pe pozitii impare
+ * 6. numarul elementelor divizibile cu 5
+ * daca elementul de pe pozitia p (p citit de la tastatura) este par
  */
 
 #include <iostream>
 
 using namespace std;
 
-void citesteVector (int v[], int &n)
+void afiseazaElementeVector (int v[], int n)
 {
-    int i = 0, value = 0;
-    cout << "Introduceti elementele, \'-1\' pentru a termina operatiunea:" << endl;
-    while ( value != -1)
+    cout << "Displaying array ... " << endl;
+    for(int i = 0; i < n; i++)
     {
-        cout << "Enter v[" << i << "]= ";
-        cin >> value;
-        v[i] = value;
-        i++;
+        cout << "v[" << i << "] = " << v[i] << endl;
+
     }
 }
 
-void afiseazaVector (int *p, int n)
+void afiseazaPozitiiPare (int v[], int n)
 {
-    cout << "Afisare vector: " << endl;
-    for (int i = 0; i < n; i++)
+    cout << "Displaying even indexes ... " << endl;
+    for(int i = 0; i < n; i++)
     {
-        cout << *(p+i) << " ";
+        if(i % 2 == 0)
+        {
+            cout << "v[" << i << "] = " << v[i] << endl;
+        }
     }
-    cout << endl;
+}
+
+void afiseazaPozitiiImpare (int *ptr, int n)
+{
+    cout << "Displaying odd indexes ... " << endl;
+    for(int i = 0; i < n; i++)
+    {
+        if(i % 2 != 0)
+        {
+            cout << "v[" << i << "] = " << *(ptr + i) << endl;
+        }
+    }
+}
+
+void afiseazaSumaElemPozitive (int v[], int n)
+{
+    int sum = 0;
+    for(int i = 0; i < n; i++)
+    {
+        if(v[i] > 0)
+        {
+            sum = sum + v[i];
+        }
+    }
+
+    cout << "Sum of positive elements is = " << sum << endl;
+}
+
+void afiseazaProdusElemNeg (int v[], int n)
+{
+    int prod, k; // k = counter for negative elements
+    prod = 1;
+    k = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        if(v[i] < 0)
+        {
+            k++;
+            prod = prod * v[i];
+        }
+    }
+
+    if(k == 0)
+    {
+        cout << "Product of negative elements is 0. " << endl;
+        prod = 0;
+    }
+    else
+    {
+        cout << "Product of negative elements is " << prod << endl;
+    }
+}
+
+afiseazaNrElemDiv5 (int *ptr, int n)
+{
+    int k = 0;
+
+    for(int i = 0; i < n; i++)
+    {
+        if(*(ptr+i) % 5 == 0)
+            k++;
+    }
+
+    cout << "There are " << k << " that divides with 5." << endl;
+}
+
+void afiseazaPozP (int v[], int &p)
+{
+    if(v[p] % 2 == 0)
+        cout << "v[" << p << "] = " << v[p] << " este par." << endl;
+    else
+        cout << "v[" << p << "] = " << v[p] << " este impar." << endl;
 }
 
 int main(void)
 {
-    int v[] = {0}, n;
-
-    int p* = v;
-
-    citesteVector(p, n);
-    afiseazaVector(p, n);
+    int n, p = -1;
     
+    cout << "Cate elemente doriti in vector?" << endl;
+    cout << "n = ";
+    cin >> n;
+
+    int v[n];
+    int *ptrV = v;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "v[" << i << "] = ";
+        cin >> v[i];
+    }
+
+    while (p < 0 || p >= n)
+    {
+        cout << "Alegeti un index intre 0 si " << n -1 << "." << endl;
+        cout << "Index = ";
+        cin >> p;
+    }
+
+    afiseazaElementeVector(v, n);
+    afiseazaPozitiiPare(v, n);
+    afiseazaPozitiiImpare(ptrV ,n);
+    afiseazaSumaElemPozitive(v, n);
+    afiseazaProdusElemNeg(v, n);
+    afiseazaNrElemDiv5(ptrV, n);
+    afiseazaPozP(v, p);
+
     return 0;
 }
