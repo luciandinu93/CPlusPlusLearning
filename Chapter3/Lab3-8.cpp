d2fe48155bb38061b646a23a3e29d88703d24943
Lucian Dinu <lucian.dinu93@yahoo.com>
2019-02-04 21:08:35 +0200

    Prime numbers

---
+++ Chapter3/Lab3-8.cpp
@@ -0,0 +1,29 @@
+/* Prime numbers */
+
+#include <iostream>
+
+using namespace std;
+
+bool isPrime(int num)
+{
+    if(num == 0 || num == 1)
+        return 0;
+
+    for(int i = 2; i < num; i++)
+        if(num % i == 0)
+        {
+            return 0;
+        }
+
+    return 1;
+}
+
+int main(void)
+{
+    for(int i = 0; i <= 21; i++)
+        if(isPrime(i))
+            cout << i << " ";
+    cout << endl;
+
+    return 0;
+}
