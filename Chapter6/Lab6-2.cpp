81ccb0588522f016519dd1a931d9189d69140c17
Lucian Dinu <lucian.dinu93@yahoo.com>
2019-03-17 15:51:05 +0200

    Lab6-2.cpp

--- Chapter6/Lab6-2.cpp
+++ Chapter6/Lab6-2.cpp
@@ -1,41 +1,112 @@
 /* Polymorphism part 2 */
 
 #include <iostream>
+#include <string>
+#include <cmath>
 
-class BaseDraw
+class Piece
 {
 public:
-	virtual void Draw() {std::cout << "BaseDraw:\n /\\";}
+	Piece() {this->x_int = 1; this->y_char = 'b';}
+	void displayPosition(void);
+	virtual bool check(char, int);
+	virtual std::string getType(void);
+protected:
+	// position
+	int x_int;
+	char y_char;
 };
 
-class Draw1 : public BaseDraw
+class Man : public Piece
 {
-public:
-	void Draw() {std::cout << "Drawing1:\n /\\ \n//\\\\" << std::endl;}
+	bool check(char, int);
+	std::string getType(void);
 };
 
-class Draw2 : public BaseDraw
+class King : public Piece
 {
-public:
-	void Draw() {std::cout << "Drawing2:\n /\\ \n/**\\" << std::endl;}
+	bool check(char, int);
+	std::string getType(void);
 };
 
-class Draw3 : public BaseDraw
+void Piece::displayPosition(void)
 {
-public:
-	void Draw() {std::cout << "Drawing3:\n /\\ \n/++\\" << std::endl;}
-};
+	std::cout << "Position of the piece is : *" << this->y_char << " " << this->x_int << "*" << std::endl;
+}
 
+bool Piece::check(char y, int x)
+{
+	std::cout << "virtual function" << std::endl;
+	return false;
+}
+
+std::string Piece::getType(void)
+{
+	return "Piece";
+}
+
+bool King::check(char y, int x)
+{
+	if(y < 'a')
+		throw "Invalid position";
+	if(x < 1)
+		throw "Invalid position";
+	if(y_char - y == 0 != x_int - x == 0)
+		return true;
+	
+	return false;
+}
+
+std::string King::getType(void)
+{
+	return "King";
+}
+
+bool Man::check(char y, int x)
+{
+	if(y < 'a')
+		throw "Invalid position";
+	if(x < 1)
+		throw "Invalid position";
+	if(std::abs(y_char - y) == std::abs(x_int - x))
+		return true;
+	
+	return false;
+}
+
+std::string Man::getType(void)
+{
+	return "Man";
+}
+
+void move(Piece* p, char y, int x)
+{
+	if(p->check(y, x))
+	{
+		std::cout << "Piece of type " << p->getType() << " can be moved at position *" << y << " " << x << "*" << std::endl;
+	}
+	else
+	{
+		std::cout << "Piece of type " << p->getType() << " can not be moved at position *" << y << " " << x << "*" << std::endl;
+	}
+}
 int main(void)
 {
-	BaseDraw *draws[3];
+	Piece *k = new King;
+	Piece *m = new Man;
+
+	// Positive test
+	move(k, 'f', 1);
+	move(m, 'e', 4);
 	
-	draws[0] = new Draw1();
-	draws[1] = new Draw2();
-	draws[2] = new Draw3();
+	std::cout << std::endl;
 	
-	for(int i = 0; i < 3; i++)
-		draws[i]->Draw();
+	// Negative test
+	move(k, 'e', 2);
+	move(m, 'd', 6);
+
+	delete k;
+	delete m;
 	
 	return 0;
 }
